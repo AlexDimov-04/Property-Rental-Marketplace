dd5e00ea99949b6e03f58cb9ae2aef5e282caa99
Alexander Dimov <alex.lemuel@gmail.com>
2023-07-07 22:25:43 +0300

    reformed user authentication code logic

--- property_rental_marketplace/property_rental_marketplace/user_authentication/views.py
+++ property_rental_marketplace/property_rental_marketplace/user_authentication/views.py
@@ -1,64 +1,59 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.views import View
+from property_rental_marketplace.user_authentication.decorators import allowed_users
+from property_rental_marketplace.user_authentication.decorators import unauthenticated_user_restricted
 from property_rental_marketplace.user_authentication.forms import UserRegistrationForm
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import views as auth_views
+from django.contrib import messages
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic as views
+from django.utils.decorators import method_decorator
 
 # 1 week in seconds
 SESSION_EXPIRATION_TIME = 604_800
 
 @login_required(login_url="sign_in")
+@allowed_users(allowed_roles=['admin'])
 def index(request):
     if request.session.get_expiry_age() == SESSION_EXPIRATION_TIME:
+        print(SESSION_EXPIRATION_TIME)
         messages.info(request, "Session has expired. Please log in again.")
 
     return render(request, "home/index.html")
 
-
-class RegisterView(View):
+@method_decorator(unauthenticated_user_restricted, name='dispatch')
+class RegisterView(views.CreateView):
     template_name = "authentication/register.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("sign_in")
 
-    def post(self, request):
-        form = UserRegistrationForm(request.POST)
-        checkbox = request.POST.get("privacy_policy")
+    def form_valid(self, form):
+        checkbox = self.request.POST.get("privacy_policy")
 
-        if form.is_valid():
-            if not checkbox:
-                messages.info(
-                    request, "You must accept the privacy policy to register!"
-                )
-                return render(request, self.template_name, {"form": form})
-
-            user = form.save(commit=False)
-            user.first_name = form.cleaned_data["first_name"]
-            user.last_name = form.cleaned_data["last_name"]
-
-            messages.success(
-                request, "User: " + user.username + " successfully created an account!"
+        if not checkbox:
+            messages.info(
+                self.request, "You must accept the privacy policy to register!"
             )
-            user.save()
+            return self.render_to_response(self.get_context_data(form=form))
 
-            return redirect("sign_in")
+        user = form.save(commit=False)
+        messages.success(
+            self.request, "User: " + user.username + " successfully created an account!"
+        )
 
-        return render(request, self.template_name, {"form": form})
+        return super().form_valid(form)
 
-    def get(self, request):
-        if request.user.is_authenticated:
-            return redirect("index")
-
-        form = UserRegistrationForm()
-
-        for field in form.fields.values():
-            field.error_messages = {}
-
-        return render(request, self.template_name, {"form": form})
-
-
-class SignInView(View):
+@method_decorator(unauthenticated_user_restricted, name='dispatch')
+class SignInView(auth_views.LoginView):
     template_name = "authentication/login.html"
+    next_page = reverse_lazy('index')
 
-    def post(self, request):
+    def form_invalid(self, form):
+        messages.info(self.request, "Incorrect password or username!")
+        return super().form_invalid(form)
+
+    def post(self, request, *args, **kwargs):
         username = request.POST.get("username")
         password = request.POST.get("password")
         remember_me = request.POST.get("remember_me")
@@ -70,18 +65,11 @@ class SignInView(View):
             if remember_me:
                 request.session.set_expiry(SESSION_EXPIRATION_TIME)  
 
-            return redirect("index")
+            return super().post(request, *args, **kwargs)
         else:
             messages.info(request, "Incorrect password or username!")
             return render(request, self.template_name)
 
-    def get(self, request):
-        if request.user.is_authenticated:
-            return redirect("index")
 
-        return render(request, self.template_name)
-
-
-def sign_out(request):
-    logout(request)
-    return redirect("sign_in")
+class SignOutView(auth_views.LogoutView):
+    next_page = reverse_lazy("sign_in")
