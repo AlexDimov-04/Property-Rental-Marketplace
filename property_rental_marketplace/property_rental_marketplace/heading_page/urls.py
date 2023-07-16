03118e8b30c7c5522284eb5070fc0f29db9402c7
Alexander Dimov <alex.lemuel@gmail.com>
2023-07-16 22:27:04 +0300

    added url for the update view

--- property_rental_marketplace/property_rental_marketplace/heading_page/urls.py
+++ property_rental_marketplace/property_rental_marketplace/heading_page/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('profile-details/', views.UserProfileView.as_view(), name='profile_details')
+    path('profile-details/', views.UserProfileView.as_view(), name='profile_details'),
+    path('update-profile/', views.UserProfileUpdateView.as_view(), name='update_profile'),
 ]
