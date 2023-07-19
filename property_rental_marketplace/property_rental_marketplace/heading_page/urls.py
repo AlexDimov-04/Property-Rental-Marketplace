c897f47d5da6b3341d2b12c79c5babf5af755eeb
Alexander Dimov <alex.lemuel@gmail.com>
2023-07-19 03:53:17 +0300

    encapsulated list for each profile operation

--- property_rental_marketplace/property_rental_marketplace/heading_page/urls.py
+++ property_rental_marketplace/property_rental_marketplace/heading_page/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+profile_denpendencies = [
+    path('details/', views.UserProfileView.as_view(), name='profile_details'),
+    path('update/', views.UserProfileUpdateView.as_view(), name='update_profile'),
+    path('delete/', views.UserProfileDeleteView.as_view(), name='delete_profile')
+]
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('profile-details/', views.UserProfileView.as_view(), name='profile_details'),
-    path('update-profile/', views.UserProfileUpdateView.as_view(), name='update_profile'),
+    path('profile/', include(profile_denpendencies))
 ]
