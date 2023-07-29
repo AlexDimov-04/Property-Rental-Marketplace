f496d1eaa08fe3b9bc4a2cf787b3387988a70bab
Alexander Dimov <alex.lemuel@gmail.com>
2023-07-30 00:28:50 +0300

    added placeholders for each property form field

--- property_rental_marketplace/property_rental_marketplace/property_market/forms.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/forms.py
@@ -1,26 +1,50 @@
 from django import forms
-from .models import BaseProperty ,Apartment, Villa, Office, Shop, Building
+from .models import BaseProperty, Apartment, Villa, Office, Shop, Building
+
 
 class BasePropertyForm(forms.ModelForm):
     class Meta:
         model = BaseProperty
-        exclude = ('property',)
+        exclude = ("property",)
+
+        widgets = {
+            "price": forms.NumberInput(
+                attrs={"placeholder": "Price"}
+            ),
+
+            "title": forms.TextInput(
+                attrs={"placeholder": "Title"}
+            ),
+
+            "location": forms.TextInput(
+                attrs={"placeholder": "Location"}
+            ),
+
+            "images": forms.ClearableFileInput(
+                attrs={"placeholder": "Image"}
+            ),
+        }
+
 
 class ApartmentForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Apartment
 
+
 class VillaForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Villa
+
 
 class OfficeForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Office
 
+
 class ShopForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Shop
+
 
 class BuildingForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
