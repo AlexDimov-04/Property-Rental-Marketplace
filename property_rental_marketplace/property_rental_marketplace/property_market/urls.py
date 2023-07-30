2b868871e8bb702e8b106bcfcc7cda3ff6343eb3
Alexander Dimov <alex.lemuel@gmail.com>
2023-07-30 18:13:02 +0300

    added method for dynamically forms rendering

--- property_rental_marketplace/property_rental_marketplace/property_market/urls.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from property_rental_marketplace.property_market.views import PropertyListView, PropertyCreateView
+from property_rental_marketplace.property_market.views import PropertyListView, PropertyCreateView \
+    ,get_additional_form_fields
+
 
 
 urlpatterns = [
     path('list-properties/', PropertyListView.as_view(), name='property_list'),
     path('create-properties/', PropertyCreateView.as_view(), name='property_create'),
+    path('get_additional_form_fields/', get_additional_form_fields, name='additional_form_fields')
 ]
