50580652d6e7f79aa4499636db8cfca6673cc426
Alexander Dimov <alex.lemuel@gmail.com>
2023-07-30 18:11:39 +0300

    added models to the admin manager

--- property_rental_marketplace/property_rental_marketplace/property_market/admin.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from property_rental_marketplace.property_market.models import BaseProperty, Apartment \
+    ,Villa, Office, Shop, Building
 
-# Register your models here.
+@admin.register(BaseProperty)
+class BasePropertyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Villa)
+class VillaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Office)
+class OfficeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    pass
