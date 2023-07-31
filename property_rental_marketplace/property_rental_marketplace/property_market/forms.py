2d3a77bde3b19f95b4ea804bfaa47dfd2c561641
Alexander Dimov <alex.lemuel@gmail.com>
2023-07-31 17:33:57 +0300

    Added placeholder in forms for each property type

--- property_rental_marketplace/property_rental_marketplace/property_market/forms.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/forms.py
@@ -30,22 +30,108 @@ class ApartmentForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Apartment
 
+        widgets = {
+            "description": forms.TextInput(
+                attrs={"placeholder": "Apartment Description"}
+            ),
+
+            "num_bedrooms": forms.NumberInput(
+                attrs={"placeholder": "Bedrooms"}
+            ),
+
+            "num_bathrooms": forms.NumberInput(
+                attrs={"placeholder": "Bathrooms"}
+            ),
+
+            "area_sqft": forms.NumberInput(
+                attrs={"placeholder": "Area Sqft"}
+            ),
+
+            "floor_number": forms.NumberInput(
+                attrs={"placeholder": "Floor Number"}
+            ),
+        }
+
 
 class VillaForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Villa
+
+        widgets = {
+            "description": forms.TextInput(
+                attrs={"placeholder": "Villa Description"}
+            ),
+
+            "num_bedrooms": forms.NumberInput(
+                attrs={"placeholder": "Bedrooms"}
+            ),
+
+            "num_bathrooms": forms.NumberInput(
+                attrs={"placeholder": "Bathrooms"}
+            ),
+
+            "area_sqft": forms.NumberInput(
+                attrs={"placeholder": "Area Sqft"}
+            ),
+
+            "floor_number": forms.NumberInput(
+                attrs={"placeholder": "Floor Number"}
+            ),  
+        }
 
 
 class OfficeForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Office
 
+        widgets = {
+            "description": forms.TextInput(
+                attrs={"placeholder": "Office Description"}
+            ),
+
+            "num_rooms": forms.NumberInput(
+                attrs={"placeholder": "Rooms"}
+            ),
+
+            "area_sqft": forms.NumberInput(
+                attrs={"placeholder": "Area Sqft"}
+            ),
+
+            "floor_number": forms.NumberInput(
+                attrs={"placeholder": "Floor Number"}
+            ),  
+        }
+
 
 class ShopForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Shop
 
+        widgets = {
+            "description": forms.TextInput(
+                attrs={"placeholder": "Shop Description"}
+            ),
+
+            "area_sqft": forms.NumberInput(
+                attrs={"placeholder": "Area Sqft"}
+            ),
+        }
+
 
 class BuildingForm(BasePropertyForm):
     class Meta(BasePropertyForm.Meta):
         model = Building
+
+        widgets = {
+            "description": forms.TextInput(
+                attrs={"placeholder": "Building Description"}
+            ),
+
+            "num_floors": forms.NumberInput(
+                attrs={"placeholder": "Number Of Floors"}
+            ),
+
+            "total_area_sqft": forms.NumberInput(
+                attrs={"placeholder": "Total Area Sqft"}
+            ),  
+        }
