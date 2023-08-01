82598834b66ce0813ae88bef35557dc5cc49fc3a
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-01 04:02:04 +0300

    erased empty lines in urls.py

--- property_rental_marketplace/property_rental_marketplace/property_market/urls.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from property_rental_marketplace.property_market.views import PropertyListView, PropertyCreateView \
     ,get_additional_form_fields
 
-
-
 urlpatterns = [
     path('list-properties/', PropertyListView.as_view(), name='property_list'),
     path('create-properties/', PropertyCreateView.as_view(), name='property_create'),
