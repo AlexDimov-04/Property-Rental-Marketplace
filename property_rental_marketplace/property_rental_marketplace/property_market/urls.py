1359deb373c23896b350b9ef9786075877069683
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-01 19:24:51 +0300

    added a detail view url

--- property_rental_marketplace/property_rental_marketplace/property_market/urls.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from property_rental_marketplace.property_market.views import PropertyListView, PropertyCreateView \
-    ,get_additional_form_fields
+    ,get_additional_form_fields, PropertyDetailsView
 
 urlpatterns = [
     path('list-properties/', PropertyListView.as_view(), name='property_list'),
     path('create-properties/', PropertyCreateView.as_view(), name='property_create'),
-    path('get_additional_form_fields/', get_additional_form_fields, name='additional_form_fields')
+    path('get_additional_form_fields/', get_additional_form_fields, name='additional_form_fields'),
+    path('property/<int:pk>/', PropertyDetailsView.as_view(), name='property_details')
 ]
