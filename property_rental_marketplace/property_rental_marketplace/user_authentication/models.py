938742da68e6cf7333fdd76b07850452197ad349
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-02 18:07:07 +0300

    added phone field + strong phone regex validator

--- property_rental_marketplace/property_rental_marketplace/user_authentication/models.py
+++ property_rental_marketplace/property_rental_marketplace/user_authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 class UserProfile(models.Model):
 
@@ -8,6 +9,11 @@ class UserProfile(models.Model):
         ('FEMALE', 'FEMALE'),
         ('Do not specify', 'Do not specify')
     ]
+
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+    )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -43,6 +49,14 @@ class UserProfile(models.Model):
     country = models.CharField(
         null=True,
         blank=True
+    )
+
+    phone = models.CharField(
+        validators=[phone_regex],
+        max_length=17,  # Adjust the max length to fit your phone numbers (e.g., '+999999999')
+        null=True,
+        blank=True,
+        help_text='Contact Phone Number'
     )
 
     bio = models.TextField(
