e82c6cdca1d8b10c90bb697a51ea1650eca352e3
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-03 03:49:38 +0300

    added update and delete view + encapsulate include

--- property_rental_marketplace/property_rental_marketplace/property_market/urls.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from property_rental_marketplace.property_market.views import PropertyListView, PropertyCreateView \
-    ,get_additional_form_fields, PropertyDetailsView
+    ,get_additional_form_fields, PropertyDetailsView, PropertyUpdateView, PropertyDeleteView
+
+properties_operations = [
+    path('', PropertyDetailsView.as_view(), name='property_details'),
+    path('update/', PropertyUpdateView.as_view(), name='property_update'),
+    path('delete/', PropertyDeleteView.as_view(), name='property_delete')
+]
 
 urlpatterns = [
     path('list-properties/', PropertyListView.as_view(), name='property_list'),
     path('create-properties/', PropertyCreateView.as_view(), name='property_create'),
     path('get_additional_form_fields/', get_additional_form_fields, name='additional_form_fields'),
-    path('property/<int:pk>/', PropertyDetailsView.as_view(), name='property_details')
+    path('property/<int:pk>/', include(properties_operations))
 ]
