8493eb1d482957ca6688da5c501154a5e557b87f
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-05 04:06:01 +0300

    changed smtp config to handle password change

--- property_rental_marketplace/property_rental_marketplace/settings.py
+++ property_rental_marketplace/property_rental_marketplace/settings.py
@@ -145,8 +145,9 @@ LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = 'sign_in'
 
 # SMTP Configuration
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alex.lemuel@gmail.com'
-EMAIL_HOST_PASSWORD = 'fjwyuxcigumlxijo'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'alex.lemuel@gmail.com'
+EMAIL_HOST_PASSWORD = 'tltotyltanuuxwve'
