020da313bfc6960f988f5d270ca0eb974e4356ea
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-05 04:07:15 +0300

    add builtin django views for pass-email validation

--- property_rental_marketplace/property_rental_marketplace/profile_management/urls.py
+++ property_rental_marketplace/property_rental_marketplace/profile_management/urls.py
@@ -1,11 +1,32 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from property_rental_marketplace.property_market.views import PropertyListView
 from . import views
 
 profile_denpendencies = [
     path('details/', views.UserProfileView.as_view(), name='profile_details'),
     path('update/', views.UserProfileUpdateView.as_view(), name='update_profile'),
-    path('delete/', views.UserProfileDeleteView.as_view(), name='delete_profile')
+    path('delete/', views.UserProfileDeleteView.as_view(), name='delete_profile'),
+    path(
+        'reset-password/', 
+        auth_views.PasswordResetView.as_view(template_name="passwords/password_reset.html"), 
+        name='reset_password'
+    ),
+    path(
+        'reset-password-sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="passwords/password_reset_sent.html"), 
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name="passwords/password_reset_form.html"), 
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset-password-complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="passwords/password_reset_done.html"), 
+        name='password_reset_complete'
+    ),
 ]
 
 urlpatterns = [
