f35b3eb78c65d5eeda7bac79e32e520b535757a9
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-06 21:01:40 +0300

    added custom admin url for s3 bucket

--- property_rental_marketplace/property_rental_marketplace/profile_management/admin.py
+++ property_rental_marketplace/property_rental_marketplace/profile_management/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from property_rental_marketplace.settings import AWS_S3_CUSTOM_DOMAIN
 
-# Register your models here.
+class CustomAdminSite(admin.AdminSite):
+    static_url = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
