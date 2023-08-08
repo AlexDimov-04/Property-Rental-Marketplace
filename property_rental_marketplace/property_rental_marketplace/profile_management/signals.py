f4723010c43f3b71306d6934fb9e47960faa420b
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-08 21:32:25 +0300

    added signal for a notification on each added news

---
+++ property_rental_marketplace/property_rental_marketplace/profile_management/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from property_rental_marketplace.profile_management.models import NewsletterFollower, AdminNewsLetterPost
+
+@receiver(post_save, sender=AdminNewsLetterPost)
+def send_notification_to_followers(sender, instance, **kwargs):
+    followers = NewsletterFollower.objects.all()
+    subject = "New Newsletter Post!"
+    message = f"A new newsletter post has been published: {instance.content}. Check it out on the About page."
+    from_email = "rentwise.marketplace.gmail.com"
+
+    for follower in followers:
+        send_mail(subject, message, from_email, [follower.email])
