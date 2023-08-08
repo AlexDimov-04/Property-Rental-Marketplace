0b3cfb7d76cf9cad844243c486980be4084c96a1
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-08 21:35:53 +0300

    added UserComment Admin model

--- property_rental_marketplace/property_rental_marketplace/user_authentication/admin.py
+++ property_rental_marketplace/property_rental_marketplace/user_authentication/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from property_rental_marketplace.user_authentication.models import UserProfile
+from property_rental_marketplace.user_authentication.models import UserProfile, UserComment
 
 @admin.register(UserProfile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserComment)
+class CommentAdmin(admin.ModelAdmin):
     pass
