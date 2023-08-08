7a5765c8cba4e9a1a6c1fa9a2028629c630da8c2
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-08 05:13:16 +0300

    added 2 new models for contacts and letter posts

--- property_rental_marketplace/property_rental_marketplace/profile_management/models.py
+++ property_rental_marketplace/property_rental_marketplace/profile_management/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.subject
+    
+class NewsletterFollower(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+    
+class AdminNewsLetterPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
