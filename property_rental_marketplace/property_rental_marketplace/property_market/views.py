899d5f357fa3f0b4dc3adfbf13b9a6d02ab7fec4
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-09 21:51:32 +0300

    added the views that handles payment stripe logic

--- property_rental_marketplace/property_rental_marketplace/property_market/views.py
+++ property_rental_marketplace/property_rental_marketplace/property_market/views.py
@@ -1,4 +1,7 @@
-from django.http import JsonResponse
+import time
+import stripe
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
@@ -8,6 +11,9 @@ from property_rental_marketplace.property_market.models import BaseProperty, Sav
 from property_rental_marketplace.profile_management.views import UserProfileMixin
 from property_rental_marketplace.property_market.forms import BasePropertyForm, ApartmentForm \
     ,VillaForm, OfficeForm, ShopForm, BuildingForm, SavePropertyForm, PropertyEstimateForm
+from property_rental_marketplace.user_authentication.models import UserProfile
+from property_rental_marketplace.user_authentication.models import UserPayment
+from django.views.decorators.csrf import csrf_exempt
 
 PROPERTY_TYPE_MAPPING = {
     'Apartment': {
@@ -292,3 +298,69 @@ class EstimatePropertyResultView(UserProfileMixin, views.TemplateView):
         context['estimated_value'] = PropertyEstimate.objects.last()
 
         return context
+    
+def product_page(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    if request.method == "POST":
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "price": 'price_1NdC33INgkhsKPqSYPk3VQ2u',
+                    "quantity": 1,
+                },
+            ],
+            mode="payment",
+            customer_creation="always",
+            success_url=settings.REDIRECT_DOMAIN
+            + "/properties/payment/successful/?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=settings.REDIRECT_DOMAIN + "/properties/payment/cancelled/",
+        )
+        print(property_instance)
+        return redirect(checkout_session.url, code=303)
+    return render(request, "payments/payment_page.html")
+
+def payment_successful(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    checkout_session_id = request.GET.get("session_id", None)
+    session = stripe.checkout.Session.retrieve(checkout_session_id)
+    customer = stripe.Customer.retrieve(session.customer)
+
+    user_profile = UserProfile.objects.get(user=request.user)
+    user_payment, created = UserPayment.objects.get_or_create(user_profile=user_profile)
+
+    user_payment.stripe_checkout_id = checkout_session_id
+    user_payment.payment_bool = True
+    user_payment.save()
+    
+    return render(
+        request, "payments/payment_successful.html", {"customer": customer}
+    )
+
+def payment_cancelled(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    return render(request, "payments/payment_cancelled.html")
+
+@csrf_exempt
+def stripe_webhook(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    time.sleep(10)
+    payload = request.body
+    signature_header = request.META["HTTP_STRIPE_SIGNATURE"]
+    event = None
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, signature_header, settings.STRIPE_WEBHOOK_SECRET_TEST
+        )
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+    if event["type"] == "checkout.session.completed":
+        session = event["data"]["object"]
+        session_id = session.get("id", None)
+        time.sleep(15)
+        user_payment = UserPayment.objects.get(stripe_checkout_id=session_id)
+        user_payment.payment_bool = True
+        user_payment.save()
+    return HttpResponse(status=200)
