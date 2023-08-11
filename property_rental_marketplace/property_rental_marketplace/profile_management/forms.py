655e1e1619e5072edd88ca573ab83907f1e1f756
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-11 20:55:22 +0300

    reformated file

--- property_rental_marketplace/property_rental_marketplace/profile_management/forms.py
+++ property_rental_marketplace/property_rental_marketplace/profile_management/forms.py
@@ -1,85 +1,104 @@
-from django import forms
-from property_rental_marketplace.profile_management.models import ContactMessage, NewsletterFollower, AdminNewsLetterPost
-from property_rental_marketplace.user_authentication.models import UserProfile
 from django.db.models import Q
+from django import forms
+from property_rental_marketplace.profile_management.models import (
+    ContactMessage,
+    NewsletterFollower,
+    AdminNewsLetterPost,
+)
+from property_rental_marketplace.user_authentication.models import UserProfile
+
 
 class UserProfileBaseForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        exclude = ("user",)
+
 
 class UserProfileUpdateForm(UserProfileBaseForm):
     birth_date = forms.DateField(
-        widget=forms.DateInput(
-            attrs={
-                'type': 'date', 
-                'class': 'form-control'
-                }
-            ),
-            required=False
-        )
+        widget=forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+        required=False,
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for (field_name, field) in self.fields.items():
+        for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
             field.widget.attrs["id"] = field_name
 
     def clean_email(self):
-        email = self.cleaned_data.get('email')
+        email = self.cleaned_data.get("email")
         current_user_email = self.instance.user.email
 
-        if email != current_user_email and UserProfile.objects.filter(~Q(user=self.instance.user), email=email).exists():
-            raise forms.ValidationError('This email is already in use.')
+        if (
+            email != current_user_email
+            and UserProfile.objects.filter(
+                ~Q(user=self.instance.user), email=email
+            ).exists()
+        ):
+            raise forms.ValidationError("This email is already in use.")
 
         return email
 
     def clean(self):
         cleaned_data = super().clean()
-        first_name = cleaned_data.get('first_name')
-        last_name = cleaned_data.get('last_name')
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
 
         if first_name:
             if len(first_name) < 2:
-                self.add_error("first_name", "First name should be at least 2 characters long.")
+                self.add_error(
+                    "first_name", "First name should be at least 2 characters long."
+                )
             elif any(char.isdigit() for char in first_name):
                 self.add_error("first_name", "First name cannot contain digits.")
             elif first_name[0].islower():
-                self.add_error("first_name", "First name should start with an uppercase letter.")
+                self.add_error(
+                    "first_name", "First name should start with an uppercase letter."
+                )
 
         if last_name:
             if len(last_name) < 2:
-                self.add_error("last_name", "Last name should be at least 2 characters long.")
+                self.add_error(
+                    "last_name", "Last name should be at least 2 characters long."
+                )
             elif any(char.isdigit() for char in last_name):
                 self.add_error("last_name", "Last name cannot contain digits.")
             elif last_name[0].islower():
-                self.add_error("last_name", "Last name should start with an uppercase letter.")
+                self.add_error(
+                    "last_name", "Last name should start with an uppercase letter."
+                )
 
         return cleaned_data
-    
+
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'message': forms.Textarea(attrs={'class': 'form-control'}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "subject": forms.TextInput(attrs={"class": "form-control"}),
+            "message": forms.Textarea(attrs={"class": "form-control"}),
         }
-        
+
+
 class NewsLetterSubscriberForm(forms.ModelForm):
     class Meta:
         model = NewsletterFollower
-        exclude = ('email',)
+        exclude = ("email",)
+
 
 class AdminNewsLetterPostForm(forms.ModelForm):
     class Meta:
         model = AdminNewsLetterPost
-        fields = '__all__'
+        fields = "__all__"
 
         widgets = {
-            'newsletter_type': forms.TextInput(attrs={'placeholder': 'Type newsletter'}),
-            'content': forms.Textarea(attrs={'placeholder': 'Title news content'}),
-            'url_redirect': forms.TextInput(attrs={'placeholder': 'Url to redirect'}),
+            "newsletter_type": forms.TextInput(
+                attrs={"placeholder": "Type newsletter"}
+            ),
+            "content": forms.Textarea(attrs={"placeholder": "Title news content"}),
+            "url_redirect": forms.TextInput(attrs={"placeholder": "Url to redirect"}),
         }
--- property_rental_marketplace/property_rental_marketplace/profile_management/models.py
+++ property_rental_marketplace/property_rental_marketplace/profile_management/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
@@ -8,18 +9,20 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return self.subject
-    
+
+
 class NewsletterFollower(models.Model):
     email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.email
-    
+
+
 class AdminNewsLetterPost(models.Model):
     newsletter_type = models.CharField(max_length=200)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='newsletter_images')
+    image = models.ImageField(upload_to="newsletter_images")
     url_redirect = models.URLField()
 
     def __str__(self):
