632f4489a74c7d24457b08ee93a5bde8f3636e1a
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-24 15:33:15 +0300

    added all allowed hosts

--- property_rental_marketplace/property_rental_marketplace/settings.py
+++ property_rental_marketplace/property_rental_marketplace/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     "jazzmin",
